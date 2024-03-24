1b59f01e1cfed3a604cab2fbe20e767a5061a67f
Giang Minh <dominhgiang12@gmail.com>
2024-03-24 21:44:33 +0700

    video 7

--- video2/video2.js
+++ video2/video2.js
@@ -1,2 +1,7 @@
-console.log('Hello world from javascript')
-console.log('Hello Giang')
+// console.log('Hello world from javascript')
+// console.log('Hello Giang')
+
+let c = 5;
+let d = 5.9;
+let a = 'Giang', x = 'Son'
+console.log('Gia tri: ', c, d, a, x);
