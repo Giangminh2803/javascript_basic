5bb27981bba377ac4b0d81c1b453f148f644ecfb
Giang Minh <dominhgiang12@gmail.com>
2024-03-25 11:22:44 +0700

    String methods

--- video2/video2.js
+++ video2/video2.js
@@ -1,10 +1,15 @@
 // console.log('Hello world from javascript')
 // console.log('Hello Giang')
 
-let c = 5;
-let d = 5.9;
-let a = 'Giang', x = 'Son'
-console.log('Gia tri: ', c, d, a, x);
-console.log(c + d + a);
-console.log(a + c + d);
-console.log(a.length);
+// let c = 5;
+// let d = 5.9;
+// let a = 'Giang', x = 'Son'
+// console.log('Gia tri: ', c, d, a, x);
+// console.log(c + d + a);
+// console.log(a + c + d);
+// console.log(a.length);
+let a = `1$2$3$4$5`;
+b = a.split('$')
+console.log(b[4]);
+console.log(`Hello world from HTML`)
+console.log(`Giang bac`)
